285fe3df46531cc587377358d019af42ff736938
Adam <cheradam2011@yandex.ru>
2026-04-11 00:53:05 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## Проект: My Portfolio 💼
+
+**My Portfolio** — это мой сайт-портфолио с резюме, навыками, проектами и контактами.
+
+🌐 Просмотр:
+[My Portfolio](https://adamchereshniuk.github.io/My-Portfolio/)
+
+### 🛠️ Технологии:
+
+- [React](https://react.dev/)
+- [Redux](https://react-redux.js.org/)
+- [TypeScript](https://www.typescriptlang.org/)
+- [Vite](https://vite.dev/)
---
+++ src/helpers/projectsList.js
@@ -0,0 +1,130 @@
+// import project01 from "./../img/projects/01.jpg";
+// import project01Big from "./../img/projects/01.jpg";
+
+// import project02 from "./../img/projects/02.jpg";
+// import project02Big from "./../img/projects/02.jpg";
+
+// import project03 from "./../img/projects/03.jpg";
+// import project03Big from "./../img/projects/03.jpg";
+
+// import project04 from "./../img/projects/04.jpg";
+// import project04Big from "./../img/projects/04.jpg";
+
+// import project05 from "./../img/projects/05.jpg";
+// import project05Big from "./../img/projects/05.jpg";
+
+// import project06 from "./../img/projects/06.jpg";
+// import project06Big from "./../img/projects/06.jpg";
+import lagoona from "./../img/projects/lagoona.png";
+import lagoonaBig from "./../img/projects/lagoona.png";
+
+import vk_marusya from "./../img/projects/vk_marusya.png";
+import vk_marusyaBig from "./../img/projects/vk_marusya.png";
+
+import realtime_weather_app from "./../img/projects/realtime_weather_app.png";
+import realtime_weather_appBig from "./../img/projects/realtime_weather_app.png";
+
+import crm from "./../img/projects/crm.png";
+import crmBig from "./../img/projects/crm.png";
+
+import blanchard from "./../img/projects/blanchard.png";
+import blanchardBig from "./../img/projects/blanchard.png";
+
+import spotify_clone from "./../img/projects/spotify_clone.png";
+import spotify_cloneBig from "./../img/projects/spotify_clone.png";
+
+import dashboard from "./../img/projects/dashboard.png";
+import dashboardBig from "./../img/projects/dashboard.png";
+
+import meals_app from "./../img/projects/meals_app.png";
+import meals_appBig from "./../img/projects/meals_app.png";
+
+import google_maps_app from "./../img/projects/google_maps_app.png";
+import google_maps_appBig from "./../img/projects/google_maps_app.png";
+
+import foodi from "./../img/projects/foodi.png";
+import foodiBig from "./../img/projects/foodi.png";
+
+const projects = [
+	{
+		title: "Lagoona",
+		skills: "HTML, CSS",
+		img: lagoona,
+		imgBig: lagoonaBig,
+		gitHubLink: "https://github.com/cheradam13/Lagoona",
+		liveSiteLink: "https://cheradam13.github.io/Lagoona/",
+	},
+	{
+		title: "VK Marusya",
+		skills: "ReactJS, Typescript, Redux",
+		img: vk_marusya,
+		imgBig: vk_marusyaBig,
+		gitHubLink: "https://github.com/cheradam13/VK-Marusya",
+		liveSiteLink: "https://cheradam13.github.io/VK-Marusya/",
+	},
+	{
+		title: "Realtime Weather App",
+		skills: "JavaScript, API",
+		img: realtime_weather_app,
+		imgBig: realtime_weather_appBig,
+		gitHubLink: "https://github.com/cheradam13/Realtime-Weather-App",
+		liveSiteLink: "https://cheradam13.github.io/Realtime-Weather-App/",
+	},
+	{
+		title: "CRM",
+		skills: "JavaScript",
+		img: crm,
+		imgBig: crmBig,
+		gitHubLink: "https://github.com/cheradam13/CRM",
+		liveSiteLink: "https://cheradam13.github.io/CRM/",
+	},
+	{
+		title: "Blanchard",
+		skills: "HTML, CSS, Figma",
+		img: blanchard,
+		imgBig: blanchardBig,
+		gitHubLink: "https://github.com/cheradam13/Blanchard",
+		liveSiteLink: "https://cheradam13.github.io/Blanchard/",
+	},
+	{
+		title: "Spotify 2.0",
+		skills: "ReactJS, MobX, Typescript, Vite, Sass",
+		img: spotify_clone,
+		imgBig: spotify_cloneBig,
+		gitHubLink: "https://github.com/cheradam13/Spotify-2.0",
+		liveSiteLink: "https://cheradam13.github.io/Spotify-2.0/?q=",
+	},
+	{
+		title: "Dashboard",
+		skills: "VueJS, Vite, ChartJS, Vue-chartjs",
+		img: dashboard,
+		imgBig: dashboardBig,
+		gitHubLink: "https://github.com/cheradam13/Dashboard",
+		liveSiteLink: "https://cheradam13.github.io/Dashboard/",
+	},
+	{
+		title: "Meals App",
+		skills: "ReactJS, Redux, TypeScript, Vite, Tailwind CSS",
+		img: meals_app,
+		imgBig: meals_appBig,
+		gitHubLink: "https://github.com/cheradam13/Meals-App",
+		liveSiteLink: "https://cheradam13.github.io/Meals-App/",
+	},
+	{
+		title: "Google Maps App",
+		skills: "VueJS, Vite, Bootstrap, Google Maps API",
+		img: google_maps_app,
+		imgBig: google_maps_appBig,
+		gitHubLink: "https://github.com/cheradam13/Google-Maps-App",
+		liveSiteLink: "https://cheradam13.github.io/Google-Maps-App/",
+	},
+	{
+		title: "Foodi",
+		skills: "WordPress, Elementor",
+		img: foodi,
+		imgBig: foodiBig,
+		liveSiteLink: "http://adamcher.atwebpages.com/#",
+	},
+];
+
+export {projects}
